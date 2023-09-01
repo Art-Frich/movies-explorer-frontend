048f1c4225f20e73387a3acc4edd4a4b8066b3f7
Art Frich <frich.g23@yandex.ru>
2023-09-01 17:02:48 +0500

    style: Techs response to 768px

--- src/components/others/Techs/Techs.tsx
+++ src/components/others/Techs/Techs.tsx
@@ -9,7 +9,7 @@ export default function Techs() {
         Технологии
       </h2>
       <div className='techs__row' />
-      <h3 className='techs__subtitle'>7 технологий</h3>
+      <h3 className='techs__subtitle'>8 технологий</h3>
       <p className='techs__text'>
         На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.
       </p>
