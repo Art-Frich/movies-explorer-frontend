912159ec3e9966a603721453a4e35dbea9848fcc
Art Frich <frich.g23@yandex.ru>
2023-09-01 16:43:23 +0500

    style: promo response to 768px

--- src/components/others/Promo/Promo.tsx
+++ src/components/others/Promo/Promo.tsx
@@ -3,22 +3,23 @@ import './Promo.css';
 import React from 'react';
 import { Link } from 'react-scroll';
 
+import promoLogo from '../../../images/landing-logo.svg';
+
 export default function Promo() {
   return (
     <section className='promo'>
       <div className='promo__content'>
-        <div>
-          <h1 className='promo__title'>
-            Учебный проект студента факультета Веб&nbsp;-&nbsp;разработки.
-          </h1>
-          <span className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</span>
-        </div>
+        <h1 className='promo__title'>
+          Учебный проект студента факультета Веб&#8209;разработки.
+        </h1>
+        <span className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</span>
 
         <Link to='about-project' smooth duration={500} className='promo__link'>
           <button type='button' className='promo__btn btn-reset btn-hover btn-active'>
             Узнать больше
           </button>
         </Link>
+        <img src={promoLogo} alt='символ веб-разработки' className='promo__logo' />
       </div>
     </section>
   );
