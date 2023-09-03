854a9d24ddb5f03005e4edf8cd0d37c5704cdd4f
Art Frich <frich.g23@yandex.ru>
2023-09-04 03:20:24 +0500

    style: fix saved-movies

--- src/components/others/MoviesCardList/MoviesCardList.tsx
+++ src/components/others/MoviesCardList/MoviesCardList.tsx
@@ -9,11 +9,6 @@ export default function MoviesCardList() {
       name: 'name1',
       time: 'time1',
       link: 'https://avatars.akamai.steamstatic.com/f71b0f42307a52e56cd8242d74b128b1446be1f1_full.jpg',
-    },
-    {
-      name: 'name1',
-      time: 'time1',
-      link: 'https://avatars.akamai.steamstatic.com/f71b0f42307a52e56cd8242d74b128b1446be1f1_full.jpg',
 
     },
     {
