04ea721e172db810f66160988fd9832cf3503ddb
Art Frich <frich.g23@yandex.ru>
2023-10-18 23:32:23 +0500

    refactor: replace last "any" TS

--- src/components/common/ProtectOfRoute.tsx
+++ src/components/common/ProtectOfRoute.tsx
@@ -3,23 +3,22 @@ import { Navigate } from 'react-router-dom';
 import React from 'react';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 
-interface IProtectRoute<T = {}> {
-  Element: React.ComponentType<T | any>,
+interface IProtectRoute<T> {
+  Element: (data: T) => JSX.Element,
   onlyLoggedIn?: boolean,
   data?: T,
 }
 
-export default function ProtectRoute<T = {}>(
+export default function ProtectRoute<T>(
   { Element, onlyLoggedIn, data }: IProtectRoute<T>
 ) {
   const { loggedIn } = useCurrentUser();
-
   return (
-    (onlyLoggedIn ? loggedIn : !loggedIn) ? <Element {...data} /> : <Navigate to='/' replace />
+    (onlyLoggedIn ? loggedIn : !loggedIn) ? <Element {...data as (T & JSX.IntrinsicAttributes)} /> : <Navigate to='/' replace />
   );
 }
 
 ProtectRoute.defaultProps = {
   onlyLoggedIn: false,
-  data: {},
+  data: undefined,
 };
